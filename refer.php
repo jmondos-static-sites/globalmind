d73127ef427f9d7fb0a2d93aa239eab886d4d808
John Gesimondo <john@jmondo.com>
2012-04-19 01:11:51 -0400

    initial commit

---
+++ refer.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Global Mind Designs :: Home</title>
+		<link rel="stylesheet" href="common.css" type="text/css" />
+		<link rel="stylesheet" href="common-sec.css" type="text/css" />
+		<link rel="stylesheet" href="contact-style.css" type="text/css" />
+		<style type="text/css">
+		
+		</style>
+	
+	</head>
+	<body>
+	<div class="wrapper">
+		<div class="header" onclick="location.href='index.html';" style="cursor:pointer;"></div>
+		<div id="nav">
+				<ul>
+					<li><a href="who.html">Who We Are</a></li>
+					<li><a href="why.html">Why Bamboo?</a></li>
+					<li><a href="buy.html">Buy a Shirt</a></li>
+					<li><br /></li>
+					<li><a href="contact.php">Contact Us</a></li>
+					<li><a href="refer.php" class="current">Refer a Friend</a></li>
+					<li><a href="http://twitter.com/gmdesigns">Follow Us on Twitter</a></li>
+				</ul>
+			</div>
+		<div class="content">
+<? include('included/refer_form.php'); ?>
+
+		</div>
+		
+		<div class="push"></div>
+	</div>		
+	<div class="footer">
+	Global Mind Designs<br /><span class="line2"> 508-555-6756 </span>
+
+
+	</div>
+	<!-- google analytics -->
+
+
+
+</body>
+
+</html>
